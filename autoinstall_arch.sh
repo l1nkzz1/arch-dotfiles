ab1212ce271736a21f2cd247e5e82f0c45bb20ab
l1nkzz1 <miguel2012343@gmail.com>
2026-05-14 16:31:11 -0300

    update autoinstaller script

--- autoinstall_arch.sh
+++ autoinstall_arch.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-if yay --version; then
-  printf 'yay OK\n'
-else
-  printf 'yay not installed\n'
-fi 
+echo " "
+echo "    ___   __  ____________  _____   ________________    __    __ "
+echo "   /   | / / / /_  __/ __ \/  _/ | / / ___/_  __/   |  / /   / / "
+echo "  / /| |/ / / / / / / / / // //  |/ /\__ \ / / / /| | / /   / /  "
+echo " / ___ / /_/ / / / / /_/ // // /|  /___/ // / / ___ |/ /___/ /___ "
+echo "/_/  |_\____/ /_/  \____/___/_/ |_//____//_/ /_/  |_/_____/_____/ "
 
 
-yay -S wlogout waybar alacritty ttf-hack-nerd hyprlock-git hyprpaper-git
+command -v yay &> /dev/null || { printf "yay is not installed, please install before running the autoinstaller\n"; exit 1; }
+
+yay -S wlogout waybar alacritty hyprlock-git hyprpaper-git
+sudo pacman -S ttf-hack-nerd
 
 cd
 git clone --recurse-submodules https://github.com/l1nkzz1/arch-dotfiles
@@ -16,4 +20,4 @@ cd arch-dotfiles
 [ ! -d ~/.config/hypr ] && cp -r hypr ~/.config/. || rm -rf ~/.config/hypr && cp -r hypr ~/.config/.
 [ ! -d ~/.config/waybar ] && cp -r waybar ~/.config/. || rm -rf ~/.config/waybar && cp -r waybar ~/.config/.
 [ ! -d ~/.config/wlogout ] && cp -r wlogout ~/.config/. || rm -rf ~/.config/wlogout && cp -r wlogout ~/.config/.
-[ ! -d ~/.config/alacritty/ ] && cp -r alacritty ~/.config/. || rm -rf ~/.config/wlogout && cp -r wlogout ~/.config/.
+[ ! -d ~/.config/alacritty/ ] && cp -r alacritty ~/.config/. || rm -rf ~/.config/alacritty && cp -r alacritty ~/.config/.
