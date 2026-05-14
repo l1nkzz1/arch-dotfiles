0e6a911ac524f1c326e733e2fc64c21a12dff01c
l1nkzz1 <miguel2012343@gmail.com>
2026-05-14 00:59:47 -0300

    fix: The hypr folder name was incorrect.

--- autoinstall_arch.sh
+++ autoinstall_arch.sh
@@ -13,7 +13,7 @@ cd
 git clone --recurse-submodules https://github.com/l1nkzz1/arch-dotfiles
 cd arch-dotfiles
 
-[ ! -d ~/.config/hypr ] && cp -r hyprland ~/.config/. || rm -rf ~/.config/hyprland && cp -r hyprland ~/.config/.
+[ ! -d ~/.config/hypr ] && cp -r hypr ~/.config/. || rm -rf ~/.config/hypr && cp -r hypr ~/.config/.
 [ ! -d ~/.config/waybar ] && cp -r waybar ~/.config/. || rm -rf ~/.config/waybar && cp -r waybar ~/.config/.
 [ ! -d ~/.config/wlogout ] && cp -r wlogout ~/.config/. || rm -rf ~/.config/wlogout && cp -r wlogout ~/.config/.
 [ ! -d ~/.config/alacritty/ ] && cp -r alacritty ~/.config/. || rm -rf ~/.config/wlogout && cp -r wlogout ~/.config/.
